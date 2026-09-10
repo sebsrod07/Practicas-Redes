9ac847bce1c0560f989c0a14122a564dec50644f
sebsrod07 <macigan2012mail.com>
2026-09-10 17:23:25 -0600

    P1;

--- P1.c
+++ P1.c
@@ -9,29 +9,29 @@ int main()
 void Calculadora(unsigned char ip[])
 {
     unsigned char MR[]={255,0,0,0};
-                if(ip[0]&128)
+        if(ip[0]&128)
+            {
+                if(ip[0]&64)
+                {
+                    if(ip[0]&32)
                     {
-                        if(ip[0]&64)
-                                    {
-                                        if(ip[0]&32)
-                                        {
-                                            if(ip[0]&16)
-                                                printf("Es de clase E\n");
-                                            else
-                                                printf("Es de clase D\n");
-                                        }
-                                        else
-                                        {
-                                            MR[1]=MR[2]=255;
-                                            printf("Es de clase C\n");
-                                        }
-                                    }
-                                    else                                                                                                                                                                                                                      
-                                    {
-                                        MR[1]=255;                                                                                                                                                                                      
-                                        printf("Es de clase B")                                                                                                                                                                                   
-                                    }
+                        if(ip[0]&16)
+                            printf("Es de clase E\n");
+                        else
+                            printf("Es de clase D\n");
                     }
-                    else                                                                                                                                                                                                                                                                               
-                    printf("Es de clase A\n");
+                    else
+                    {
+                        MR[1]=MR[2]=255;
+                        printf("Es de clase C\n");
+                    }
+                }
+                else                                                                                                                                                                                                                      
+                {
+                    MR[1]=255;                                                                                                                                                                                      
+                    printf("Es de clase B");                                                                                                                                                                               
+                }
+            }
+            else                                                                                                                                                                                                                                                                               
+                printf("Es de clase A\n");
   }
